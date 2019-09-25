5c90b223a7dfb6df02727a70e2d27dedce15d86c
v_sunmeijie <v_sunmeijie@baidu.com>
2019-09-25 20:37:04 +0800

    base

---
+++ app.js
@@ -0,0 +1,53 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser')
+
+var exampleRouter = require('./routes/router');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
+
+//指定 .html 后缀的文件使用的解析引擎
+app.engine('html',require('express-art-template'));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+//开发静态资源
+app.use('/public/',express.static('./public/'))
+
+// 配置 body-parser 中间件（插件，专门用来解析表单 POST 请求体）
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+// 路由配置
+app.use(exampleRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
